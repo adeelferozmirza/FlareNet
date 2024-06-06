f0fd5a8f8c37ecaed9eda3878982d34de8a9c1ca
adeelferozmirza <48312402+adeelferozmirza@users.noreply.github.com>
2024-06-06 13:30:49 +0800

    Add files via upload

---
+++ common.py
@@ -0,0 +1,167 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader
+from cifar10_models.densenet import densenet121
+import torch.nn.functional as F
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_channels, reduction_ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels // reduction_ratio, 1, bias=False),
+            nn.ReLU(),
+            nn.Conv2d(in_channels // reduction_ratio, in_channels, 1, bias=False)
+        )
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc(self.avg_pool(x))
+        max_out = self.fc(self.max_pool(x))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=kernel_size//2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
+def conv3x3(in_planes, out_planes, stride=1, padding=1, dilation=1, groups=1, bias=False):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=padding, dilation=dilation, groups=groups,bias=bias)
+
+def conv1x1(in_planes, out_planes, stride=1, bias=False):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=bias)
+
+class SEModule(nn.Module):
+    def __init__(self, channels, reduction=16):
+        super(SEModule, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc1 = nn.Conv2d(channels, channels // reduction, kernel_size=1, padding=0)
+        self.relu = nn.ReLU(inplace=True)
+        self.fc2 = nn.Conv2d(channels // reduction, channels, kernel_size=1, padding=0)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input):
+        x = self.avg_pool(input)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.sigmoid(x)
+        return input * x
+
+class FEA(nn.Module):
+    def __init__(self, channels):
+        super(FEA, self).__init__()
+        self.query_conv = nn.Conv2d(in_channels=channels, out_channels=channels // 8, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels=channels, out_channels=channels // 8, kernel_size=1)
+        self.value_conv = nn.Conv2d(in_channels=channels, out_channels=channels, kernel_size=1)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        batch, channels, height, width = x.size()
+        query = self.query_conv(x).view(batch, -1, width * height).permute(0, 2, 1)
+        key = self.key_conv(x).view(batch, -1, width * height)
+        value = self.value_conv(x).view(batch, -1, width * height)
+
+        attention = self.softmax(torch.bmm(query, key))
+        out = torch.bmm(value, attention.permute(0, 2, 1))
+        out = out.view(batch, channels, height, width)
+
+        return out + x
+
+class ContextualFPN(nn.Module):
+
+    def __init__(self, inplanes, outplanes, dilat, downsample=None, stride=1, t=1, scales=4, se=None, norm_layer=None,fea=None):
+        super(ContextualFPN, self).__init__()
+        if inplanes % scales != 0:
+            raise ValueError('Planes must be divisible by scales')
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        bottleneck_planes = inplanes * t
+        self.conv1 = conv1x1(inplanes, bottleneck_planes, stride)
+        self.bn1 = norm_layer(bottleneck_planes)
+        self.conv2 = nn.ModuleList([conv3x3(bottleneck_planes // scales, bottleneck_planes // scales,
+                                            groups=(bottleneck_planes // scales),dilation=dilat[i],
+                                            padding=1*dilat[i]) for i in range(scales)])
+        self.bn2 = nn.ModuleList([norm_layer(bottleneck_planes // scales) for _ in range(scales)])
+        self.conv3 = conv1x1(bottleneck_planes, outplanes)
+        self.bn3 = norm_layer(outplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.se = SEModule(outplanes) if se else None
+        self.downsample = downsample
+        self.stride = stride
+        self.fea = FEA(outplanes) if fea else None
+        self.scales = scales
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        xs = torch.chunk(out, self.scales, 1)
+        ys = []
+        for s in range(self.scales):
+            if s == 0:
+                ys.append(self.relu(self.bn2[s](self.conv2[s](xs[s]))))
+            else:
+                ys.append(self.relu(self.bn2[s](self.conv2[s](xs[s] + ys[-1]))))
+        out = torch.cat(ys, 1)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.se is not None:
+            out = self.se(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(identity)
+
+        out += identity
+        out = self.relu(out)
+        if self.fea is not None:
+            out = self.fea(out)
+
+        return out
+
+class Conv(nn.Module):
+    # A simple convolutional block
+    def __init__(self, c1, c2, k=1, s=1, p=None):
+        super().__init__()
+        if p is None:
+            p = (k - 1) // 2
+        self.conv = nn.Conv2d(c1, c2, k, s, p, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = nn.ReLU()
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+class SPP(nn.Module):
+    # Spatial pyramid pooling layer used in YOLOv3-SPP
+    def __init__(self, c1, c2, k=(5, 9, 13)):
+        super(SPP, self).__init__()
+        c_ = c1 // 2  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c_ * (len(k) + 1), c2, 1, 1)
+        self.m = nn.ModuleList([nn.MaxPool2d(kernel_size=x, stride=1, padding=x // 2) for x in k])
+
+    def forward(self, x):
+        x = self.cv1(x)
+        return self.cv2(torch.cat([x] + [m(x) for m in self.m], 1))
+
---
+++ flarenet.py
@@ -0,0 +1,67 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader
+from cifar10_models.densenet import densenet121
+import torch.nn.functional as F
+from common import*
+if torch.cuda.is_available():
+    device = torch.cuda.current_device()
+    gpu_name = torch.cuda.get_device_name(device)
+    print("GPU Name:", gpu_name)
+else:
+    print("No GPU available.")
+# Check if GPU is available and set the device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+
+class flarenet(nn.Module):
+    def __init__(self, num_classes=2):
+        super(flarenet, self).__init__()
+        # Load the pre-trained DenseNet121 model
+        original_model = densenet121(pretrained=False)
+        self.features = original_model.features
+        self.gradients = None
+        # SPP and Contextual FPN
+        self.spp = SPP(1024, 1024)  # Example dimensions
+        self.contextual_fpn = ContextualFPN(inplanes=1024, outplanes=1024, dilat=[1, 2, 3, 4], se=True)  # Example dimensions
+
+        # Attention modules
+        self.channel_attention = ChannelAttention(in_channels=1024)  # Adjust channels as needed
+        self.spatial_attention = SpatialAttention()
+
+        # Final classification layer
+        self.classifier = nn.Linear(1024, num_classes)  # Adjust channels as needed
+    def activations_hook(self, grad):
+        self.gradients = grad
+    def forward(self, x):
+        with torch.enable_grad():
+            features = self.features(x)
+            # print('features:', features.shape)
+            # Apply SPP and Contextual FPN
+            spp_features = self.spp(features)
+            # print('spp_features:', spp_features.shape)
+            fpn_features = self.contextual_fpn(spp_features)
+            # print('fpn_features:', fpn_features.shape)
+            # Apply attention mechanisms
+            ch_att = self.channel_attention(fpn_features)
+            sp_att = self.spatial_attention(fpn_features)
+            features = fpn_features * ch_att * sp_att
+            # print('features:', features.shape)
+
+            features.register_hook(self.activations_hook)
+        
+            # Pooling and classification
+            out = F.relu(features, inplace=True)
+            out = F.adaptive_avg_pool2d(out, (1, 1))
+            out = torch.flatten(out, 1)
+            out = self.classifier(out)
+            return out
+    def get_activations_gradient(self):
+        return self.gradients
+
+    def get_activations(self, x):
+        return self.features(x)
